ce8dc90defbcfa2d287a5a44753a5f132de36f59
wangyang <404012581@qq.com>
2016-05-12 17:34:50 +0800

    服务器掉线之后重新连接服务器测试成功

--- netchat/chatclient/clientdialog.cpp
+++ netchat/chatclient/clientdialog.cpp
@@ -15,6 +15,11 @@ ClientDialog::ClientDialog(QWidget *parent) :
     getServer->bind(QHostAddress::Any,7777);
     haveGetServerInfo = false;
 
+    //创建连接服务器的tcp套接子
+    interactWithServer = new QTcpSocket();
+    QObject::connect(interactWithServer,SIGNAL(connected()),this,SLOT(slot_connected_to_server()));
+    QObject::connect(interactWithServer,SIGNAL(disconnected()),this,SLOT(slot_disconnected_to_server()));
+
 
 }
 //接收服务器发来的广播消息---端口号
@@ -33,35 +38,52 @@ void ClientDialog::slotGetServerPort()
     QStringList detailMsg = msg.split('\n');
 
     QString temp = detailMsg.at(0);
-    if(temp != "CHATSERVER/1.0")
+    if(temp != "CHATSERVER/1.0" )
     {
         getServer->readAll();
-        qDebug()<<"非chatsercer 协议包: \t"<<temp;
+        qDebug()<<"dicard package!!!";
         return;
     }
 
     temp = detailMsg.at(2);
-
     temp = temp.split(" ").at(1);
     quint32 recv_len = temp.toUInt(&ok,10);
     temp = detailMsg.at(1);
 
-    if(temp.toUInt(&ok,10) == ServerAddress)
-            return handle_serveraddress(recv_len);
-   // serverPort = port.toUShort(&ok,10);
-    //qDebug()<<"get server port : "<<serverPort;
+    if(temp.toUInt(&ok,10) == ServerAddress )
+    {
+        if (haveGetServerInfo ==  false)
+        {
+                return handle_serveraddress(recv_len);
+         }
+     }
 }
 void ClientDialog::handle_serveraddress(quint32 size)
 {
-    QByteArray recvMsg;
 
+    QByteArray recvMsg;
     bool ok;
-   recvMsg.resize(getServer->pendingDatagramSize());
-   getServer->readDatagram(recvMsg.data(),recvMsg.size());
+
+   recvMsg.resize(size);
+   //数据不正确抛弃数据包
+   if(size != getServer->readDatagram(recvMsg.data(),recvMsg.size()))
+       return;
+   qDebug()<<"handle recv msg:  "<<QString(recvMsg);
    QStringList temp = QString(recvMsg).split('#');
    serverIP = temp.at(0);
    serverPort = temp.at(1).toUInt(&ok,10);
    qDebug()<<"serverIP: "<<serverIP<<"\tsereverPort: "<<serverPort;
+   interactWithServer->connectToHost(serverIP,serverPort);
+}
+void ClientDialog::slot_connected_to_server()
+{
+    qDebug()<<"connect successful!!";
+    haveGetServerInfo = true;
+}
+void ClientDialog::slot_disconnected_to_server()
+{
+    qDebug()<<"server disconnect!!";
+    haveGetServerInfo = false;
 }
 ClientDialog::~ClientDialog()
 {
--- netchat/chatclient/clientdialog.h
+++ netchat/chatclient/clientdialog.h
@@ -3,6 +3,7 @@
 
 #include <QDialog>
 #include <QUdpSocket>
+#include <QTcpSocket>
 
 namespace Ui {
     class ClientDialog;
@@ -17,13 +18,18 @@ public:
     void handle_serveraddress(quint32 size);
 public slots:
     void slotGetServerPort();
+    void slot_connected_to_server();
+     void slot_disconnected_to_server();
 
 protected:
     void changeEvent(QEvent *e);
 
 private:
     Ui::ClientDialog *ui;
+    //用于接收广播数据的套接字
     QUdpSocket *getServer;
+    //用于连接服务器的套接字
+    QTcpSocket *interactWithServer;
     quint16 serverPort ;
     QString serverIP;
     bool haveGetServerInfo;
